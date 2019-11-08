e5ccf65b51e1aee6057f9eb0ab15e3919066a4ab
Vladimir Iliev <vladimirivanoviliev@gmail.com>
2019-11-08 18:21:48 +0200

    fix: incorrect urls

---
+++ precache-manifest.8cc61bc97f5f41c2469f0650f79d96ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd4378d41b8e1f0c16f88748120dd9e",
+    "url": "/grid-test/index.html"
+  },
+  {
+    "revision": "e094c2f5abf3a183acda",
+    "url": "/grid-test/static/css/2.da2abb34.chunk.css"
+  },
+  {
+    "revision": "64d3d10132956c8377aa",
+    "url": "/grid-test/static/css/main.5767a855.chunk.css"
+  },
+  {
+    "revision": "e094c2f5abf3a183acda",
+    "url": "/grid-test/static/js/2.69318d4a.chunk.js"
+  },
+  {
+    "revision": "64d3d10132956c8377aa",
+    "url": "/grid-test/static/js/main.8fc19ae1.chunk.js"
+  },
+  {
+    "revision": "c9121809940c841ea5c3",
+    "url": "/grid-test/static/js/runtime-main.9b4b97cd.js"
+  }
+]);
